3a08539176b17705f8f64d15e8cfda3b37e4219e
Lighters_c <2362894675@qq.com>
2017-07-20 17:38:01 +0800

    Change SerialPortManagement to English

--- src/PetriTool/SerialPortManagement.java
+++ src/PetriTool/SerialPortManagement.java
@@ -36,7 +36,7 @@ public class SerialPortManagement extends JFrame {
 	public SerialPortManagement(PetriTool petriTool) {
 		setTitle("Connect to device");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 550, 600);
+		setBounds(100, 100, 600, 600);
 		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -44,63 +44,63 @@ public class SerialPortManagement extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel_Control = new JPanel();
-		panel_Control.setBorder(new TitledBorder(null, "\u63A7\u5236", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_Control.setBounds(10, 10, 177, 541);
+		panel_Control.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Control", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_Control.setBounds(10, 10, 234, 541);
 		contentPane.add(panel_Control);
 		panel_Control.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("串口选择：");
-		lblNewLabel.setBounds(10, 31, 65, 15);
+		JLabel lblNewLabel = new JLabel("Select port:");
+		lblNewLabel.setBounds(28, 34, 118, 15);
 		panel_Control.add(lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9"}));
-		comboBox.setBounds(74, 28, 78, 21);
+		comboBox.setBounds(120, 31, 78, 21);
 		panel_Control.add(comboBox);
 		
-		JLabel label = new JLabel("波特率：");
-		label.setBounds(21, 79, 54, 15);
-		panel_Control.add(label);
+		JLabel lblBaudRate = new JLabel("Baud rate:");
+		lblBaudRate.setBounds(40, 82, 70, 15);
+		panel_Control.add(lblBaudRate);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"300", "600", "1200", "2400", "4800", "9600", "19200", "38400", "43000", "56000", "57600", "115200"}));
-		comboBox_1.setBounds(74, 76, 78, 21);
+		comboBox_1.setBounds(120, 79, 78, 21);
 		panel_Control.add(comboBox_1);
 		
-		JLabel label_1 = new JLabel("停止位：");
-		label_1.setBounds(21, 127, 54, 15);
-		panel_Control.add(label_1);
+		JLabel lblStopBit = new JLabel("Stop bit:");
+		lblStopBit.setBounds(46, 130, 54, 15);
+		panel_Control.add(lblStopBit);
 		
 		JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"1", "2"}));
-		comboBox_2.setBounds(74, 124, 78, 21);
+		comboBox_2.setBounds(120, 127, 78, 21);
 		panel_Control.add(comboBox_2);
 		
-		JLabel label_2 = new JLabel("数据位：");
-		label_2.setBounds(21, 172, 54, 15);
-		panel_Control.add(label_2);
+		JLabel lblDataBits = new JLabel("Data bit:");
+		lblDataBits.setBounds(46, 175, 70, 15);
+		panel_Control.add(lblDataBits);
 		
 		JComboBox comboBox_3 = new JComboBox();
 		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"8", "7", "6", "5"}));
-		comboBox_3.setBounds(74, 169, 78, 21);
+		comboBox_3.setBounds(120, 172, 78, 21);
 		panel_Control.add(comboBox_3);
 		
-		JLabel label_3 = new JLabel("奇偶校验：");
-		label_3.setBounds(10, 218, 65, 15);
-		panel_Control.add(label_3);
+		JLabel lblOddevenCheck = new JLabel("Odd-even check:");
+		lblOddevenCheck.setBounds(10, 221, 100, 15);
+		panel_Control.add(lblOddevenCheck);
 		
 		JComboBox comboBox_4 = new JComboBox();
 		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"NONE", "ODD", "EVEN"}));
-		comboBox_4.setBounds(74, 215, 78, 21);
+		comboBox_4.setBounds(120, 218, 78, 21);
 		panel_Control.add(comboBox_4);
 		
-		JButton btn_Connect = new JButton("连接");
-		btn_Connect.setBounds(31, 276, 111, 36);
+		JButton btn_Connect = new JButton("Connect");
+		btn_Connect.setBounds(59, 281, 111, 36);
 		panel_Control.add(btn_Connect);
 		
 		JPanel panel_Send = new JPanel();
-		panel_Send.setBorder(new TitledBorder(null, "\u6570\u636E\u53D1\u9001", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_Send.setBounds(197, 10, 327, 195);
+		panel_Send.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Data sent", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_Send.setBounds(254, 10, 327, 195);
 		contentPane.add(panel_Send);
 		panel_Send.setLayout(null);
 		
@@ -111,13 +111,13 @@ public class SerialPortManagement extends JFrame {
 		jScrollPane_Send.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		jScrollPane_Send.setBounds(10, 23, 307, 119);
 		panel_Send.add(jScrollPane_Send);
-		JButton btn_Send = new JButton("发送");
+		JButton btn_Send = new JButton("Send");
 		btn_Send.setBounds(108, 152, 111, 36);
 		panel_Send.add(btn_Send);
 		
 		JPanel panel_Receive = new JPanel();
-		panel_Receive.setBorder(new TitledBorder(null, "\u6570\u636E\u63A5\u6536", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_Receive.setBounds(197, 215, 327, 195);
+		panel_Receive.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Data reception", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_Receive.setBounds(254, 215, 327, 195);
 		contentPane.add(panel_Receive);
 		panel_Receive.setLayout(null);
 		
@@ -131,8 +131,8 @@ public class SerialPortManagement extends JFrame {
 		panel_Receive.add(jScrollPane_Receive);
 		
 		JPanel panel_Identify = new JPanel();
-		panel_Identify.setBorder(new TitledBorder(null, "\u6807\u8BC6", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_Identify.setBounds(197, 420, 327, 131);
+		panel_Identify.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Marking", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_Identify.setBounds(254, 420, 327, 131);
 		contentPane.add(panel_Identify);
 		panel_Identify.setLayout(null);
 		
